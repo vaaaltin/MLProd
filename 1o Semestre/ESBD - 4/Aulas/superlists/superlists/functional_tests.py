25e8d8d146869dd11f4f0793004fd5158cb6bd8d
Valter <vammartins@inf.ufpel.edu.br>
2023-07-01 17:48:34 -0300

    Refactoring the functional testing to eliminate redundancy

--- 1o Semestre/ESBD - 4/Aulas/superlists/superlists/functional_tests.py
+++ 1o Semestre/ESBD - 4/Aulas/superlists/superlists/functional_tests.py
@@ -12,76 +12,73 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element(By.ID, 'id_list_table')
+		rows = table.find_elements(By.TAG_NAME, 'tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith ouviu falar de uma nova aplicação online interessante
 		# para lista de tarefas. Ela decide verificar a homepage
 
-		self.browser.get("http://localhost:8000")
+		self.browser.get('http://localhost:8000')
 
 		# Ela percebe que o título da página e o cabeçalho mencionam
 		# listas de tarefas (to-do)
 
-		self.assertIn("To-Do", self.browser.title)
-		header_text = self.browser.find_element(By.TAG_NAME, "h1").text
-		self.assertIn("To-Do", header_text)
+		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+		self.assertIn('To-Do', header_text)
 		
 		# Ela é convidada a inserir um item de tarefa imediatamente
 
-		inputbox = self.browser.find_element(By.ID, "id_new_item")
+		inputbox = self.browser.find_element(By.ID, 'id_new_item')
 		self.assertEqual(
-			inputbox.get_attribute("placeholder"),
-			"Enter a to-do item"
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
 		)
 
-		# Ela digita "Buy peacock feathers" (Comprar penas de pavão)
+		# Ela digita 'Buy peacock feathers' (Comprar penas de pavão)
 		# em uma nova caixa de texto (o hobby de Edith é fazer iscas
 		# para pesca com fly)
 
-		inputbox.send_keys("Buy peacock feathers")
+		inputbox.send_keys('Buy peacock feathers')
 
 
 		# Quando ela tecla enter, a página é atualizada, e agora
-		# a página lista "1 - Buy peacock feathers" como um item em 
+		# a página lista '1 - Buy peacock feathers' como um item em 
 		# uma lista de tarefas
 
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
-
-		table = self.browser.find_element(By.ID, "id_list_table")
-		rows = table.find_elements(By.TAG_NAME, "tr")
-
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# Ainda continua havendo uma caixa de texto convidando-a a 
-		# acrescentar outro item. Ela insere "Use peacock feathers 
-		# make a fly" (Usar penas de pavão para fazer um fly - 
+		# acrescentar outro item. Ela insere 'Use peacock feathers 
+		# make a fly' (Usar penas de pavão para fazer um fly - 
 		# Edith é bem metódica)
 
 		inputbox =  self.browser.find_element(By.ID, 'id_new_item')
-		inputbox.send_keys("Use peacock feathers to make a fly")
+		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 		
 
 		# A página é atualizada novamente e agora mostra os dois
 		# itens em sua lista
-		table = self.browser.find_element(By.ID, 'id_list_table')
-		rows = table.find_elements(By.TAG_NAME, 'tr')
-		self.assertIn('1: Buy peacock feathers', 
-			[row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly', 
-			[row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Buy peacock feathers')
 		time.sleep(1)
 
 		# Edith se pergunta se o site lembrará de sua lista. Então
 		# ela nota que o site gerou um URL único para ela -- há um 
 		# pequeno texto explicativo para isso.
 
-		self.fail("Finish the test!")
+		self.fail('Finish the test!')
 		
         # Ela acessa essa URL -- sua lista de tarefas continua lá.
 
 		# Satisfeita, ela volta a dormir
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	unittest.main()
