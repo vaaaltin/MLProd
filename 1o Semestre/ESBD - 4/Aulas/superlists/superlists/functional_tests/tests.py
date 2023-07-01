edc14d42069b3fd455098365652da699c28ade72
Valter <vammartins@inf.ufpel.edu.br>
2023-07-01 18:59:03 -0300

    isolamento nos testes funcionais e esperas implicitas

--- 1o Semestre/ESBD - 4/Aulas/superlists/superlists/functional_tests/tests.py
+++ 1o Semestre/ESBD - 4/Aulas/superlists/superlists/functional_tests/tests.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
 from django.test import LiveServerTestCase
 import time
-import unittest
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):    
 	def setUp(self):
@@ -12,10 +14,19 @@ class NewVisitorTest(LiveServerTestCase):
 	def tearDown(self):
 		self.browser.quit()
 
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element(By.ID, 'id_list_table')
-		rows = table.find_elements(By.TAG_NAME, 'tr')
-		self.assertIn(row_text, [row.text for row in rows])
+	# Auxiliary method 
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element(By.ID, 'id_list_table')
+				rows = table.find_elements(By.TAG_NAME, 'tr')
+				self.assertIn(row_text, [row.text for row in rows])
+				return
+			except(AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith ouviu falar de uma nova aplicação online interessante
@@ -51,7 +62,7 @@ class NewVisitorTest(LiveServerTestCase):
 
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
 		# Ainda continua havendo uma caixa de texto convidando-a a 
 		# acrescentar outro item. Ela insere 'Use peacock feathers 
@@ -66,8 +77,8 @@ class NewVisitorTest(LiveServerTestCase):
 
 		# A página é atualizada novamente e agora mostra os dois
 		# itens em sua lista
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
-		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
+		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 		time.sleep(1)
 
 		# Edith se pergunta se o site lembrará de sua lista. Então
