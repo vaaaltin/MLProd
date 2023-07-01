dfb5f18801432075ebe840e7fb202f9ff1f0d729
Valter <vammartins@inf.ufpel.edu.br>
2023-07-01 16:31:45 -0300

    Basic view now returns minimal HTML

--- 1o Semestre/ESBD - 4/Aulas/superlists/superlists/functional_tests.py
+++ 1o Semestre/ESBD - 4/Aulas/superlists/superlists/functional_tests.py
@@ -4,7 +4,7 @@ import unittest
 class NewVisitorTest(unittest.TestCase):
 
 	def setUp(self):
-		self.browser = webdriver.Firefox()
+		self.browser = webdriver.Chrome()
 
 	def tearDown(self):
 		self.browser.quit()
