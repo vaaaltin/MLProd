e3c91e933edd5bab5c94feb6aed255b4d730db33
Valter <vammartins@inf.ufpel.edu.br>
2023-07-01 17:45:18 -0300

    Estending user story to deal with a second list item

--- 1o Semestre/ESBD - 4/Aulas/superlists/superlists/functional_tests.py
+++ 1o Semestre/ESBD - 4/Aulas/superlists/superlists/functional_tests.py
@@ -57,16 +57,29 @@ class NewVisitorTest(unittest.TestCase):
 		# make a fly" (Usar penas de pavão para fazer um fly - 
 		# Edith é bem metódica)
 
-		self.fail("Finish the test!")
+		inputbox =  self.browser.find_element(By.ID, 'id_new_item')
+		inputbox.send_keys("Use peacock feathers to make a fly")
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+		
 
 		# A página é atualizada novamente e agora mostra os dois
 		# itens em sua lista
+		table = self.browser.find_element(By.ID, 'id_list_table')
+		rows = table.find_elements(By.TAG_NAME, 'tr')
+		self.assertIn('1: Buy peacock feathers', 
+			[row.text for row in rows])
+		self.assertIn('2: Use peacock feathers to make a fly', 
+			[row.text for row in rows])
+		time.sleep(1)
 
 		# Edith se pergunta se o site lembrará de sua lista. Então
 		# ela nota que o site gerou um URL único para ela -- há um 
 		# pequeno texto explicativo para isso.
 
-		# Ela acessa essa URL -- sua lista de tarefas continua lá.
+		self.fail("Finish the test!")
+		
+        # Ela acessa essa URL -- sua lista de tarefas continua lá.
 
 		# Satisfeita, ela volta a dormir
 
