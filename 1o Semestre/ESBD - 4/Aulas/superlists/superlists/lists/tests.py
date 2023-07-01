e0a59527f86b6b68a01b645b95fe64c344abbe30
Valter <vammartins@inf.ufpel.edu.br>
2023-07-01 18:09:46 -0300

    Correcting view to deal with normal request and request via POST

--- 1o Semestre/ESBD - 4/Aulas/superlists/superlists/lists/tests.py
+++ 1o Semestre/ESBD - 4/Aulas/superlists/superlists/lists/tests.py
@@ -16,8 +16,17 @@ class HomePageTest(TestCase):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
 	
+	def test_only_saves_item_when_necessary(self):
+		self.client.get('/')
+		self.assertEquals(Item.objects.count(), 0)
+
 	def test_can_save_a_POST_request(self):
 		response = self.client.post('/', data={'item_text': 'A new list item'})
+
+		self.assertEquals(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEquals(new_item.text, 'A new list item')
+
 		self.assertIn('A new list item', response.content.decode())
 		self.assertTemplateUsed(response, 'home.html')
 
