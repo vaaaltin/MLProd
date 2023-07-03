8654571f1be8a2c0f76e83158231bb61529c92e3
Valter <vammartins@inf.ufpel.edu.br>
2023-07-02 23:17:53 -0300

    URL include successful. Unit and Functional test pass.

--- 1o Semestre/ESBD - 4/Aulas/superlists/superlists/superlists/urls.py
+++ 1o Semestre/ESBD - 4/Aulas/superlists/superlists/superlists/urls.py
@@ -1,25 +1,9 @@
-"""
-URL configuration for superlists project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.urls import path, re_path
-from lists import views
+from django.conf.urls import include
+from django.urls import re_path
+from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
-    path('lists/new', views.new_list, name='new_list'),
-    re_path(r'^lists/(\d+)/$', views.view_list, name='view_list'),
-    re_path(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
+    re_path(r'^$', list_views.home_page, name='home'),
+    re_path(r'^lists/', include(list_urls)),
 ]
