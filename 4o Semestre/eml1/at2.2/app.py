22fa54df18140c74c249e95e47e7be2d27e2e6ea
vaaaltin <vammartins@inf.ufpel.edu.br>
2025-07-03 23:34:50 -0300

    operandos no get request

--- 4o Semestre/eml1/at2.2/app.py
+++ 4o Semestre/eml1/at2.2/app.py
@@ -1,16 +1,29 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
 @app.route('/soma')
 def soma():
-    x = 2
-    y = 2
+    if 'op1' in request.args:
+        x = int(request.args['op1'])
+    else:
+        return "op1 não informado", 400
+    if 'op2' in request.args:
+        y = int(request.args['op2'])
+    else:
+        return "op2 não informado", 400
     res = x + y
     return '{} + {} = {}'.format(x,y,res)
 
 @app.route('/subtracao')
 def subtracao():
-    x = 2
-    y = 2
+    if 'op1' in request.args:
+        x = int(request.args['op1'])
+    else:
+        return "op1 não informado", 400
+    if 'op2' in request.args:
+        y = int(request.args['op2'])
+    else:
+        return "op2 não informado", 400
     res = x - y
     return '{} - {} = {}'.format(x,y,res)
