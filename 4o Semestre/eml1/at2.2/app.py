d8931e94e3f284e9ab39f69aacb3ab625a0aac17
vaaaltin <vammartins@inf.ufpel.edu.br>
2025-07-03 23:52:43 -0300

    post request adjusted

--- 4o Semestre/eml1/at2.2/app.py
+++ 4o Semestre/eml1/at2.2/app.py
@@ -2,28 +2,52 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/soma')
+@app.route('/soma', methods=['GET','POST'])
 def soma():
-    if 'op1' in request.args:
-        x = int(request.args['op1'])
+    if request.method == 'GET':
+        if 'op1' in request.args:
+            x = int(request.args['op1'])
+        else:
+            return "op1 não informado", 400
+        if 'op2' in request.args:
+            y = int(request.args['op2'])
+        else:
+            return "op2 não informado", 400
     else:
-        return "op1 não informado", 400
-    if 'op2' in request.args:
-        y = int(request.args['op2'])
-    else:
-        return "op2 não informado", 400
+        request_data = request.get_json()
+        if 'op1' in request_data:
+            x = int(request_data['op1'])
+        else:
+            return "op1 não informado", 400
+        if 'op2' in request_data:
+            y = int(request_data['op2'])
+        else:
+            return "op2 não informado", 400
+
     res = x + y
     return '{} + {} = {}'.format(x,y,res)
 
-@app.route('/subtracao')
+@app.route('/subtracao', methods=['GET','POST'])
 def subtracao():
-    if 'op1' in request.args:
-        x = int(request.args['op1'])
+    if request.method == 'GET':
+        if 'op1' in request.args:
+            x = int(request.args['op1'])
+        else:
+            return "op1 não informado", 400
+        if 'op2' in request.args:
+            y = int(request.args['op2'])
+        else:
+            return "op2 não informado", 400
     else:
-        return "op1 não informado", 400
-    if 'op2' in request.args:
-        y = int(request.args['op2'])
-    else:
-        return "op2 não informado", 400
+        request_data = request.get_json()
+        if 'op1' in request_data:
+            x = int(request_data['op1'])
+        else:
+            return "op1 não informado", 400
+        if 'op2' in request_data:
+            y = int(request_data['op2'])
+        else:
+            return "op2 não informado", 400
+
     res = x - y
     return '{} - {} = {}'.format(x,y,res)
