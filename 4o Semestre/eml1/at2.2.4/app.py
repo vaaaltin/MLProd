88534cac403c08e9d43e74b1f00ffb4d3d9289db
vaaaltin <vammartins@inf.ufpel.edu.br>
2025-07-04 23:15:17 -0300

    htpp api modelo at2.1

---
+++ 4o Semestre/eml1/at2.2.4/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+
+import pickle
+app = Flask(__name__)
+
+model = pickle.load(open('model.sav', 'rb'))
+
+@app.route('/predizer_categoria', methods=['POST'])
+def predizer_categoria():
+    request_data = request.get_json()
+    input_message = [request_data['descricao']]
+    input_message = model["vect"].transform(input_message)
+    final_prediction = model["clf"].predict(input_message)[0]
+
+    response = {
+        'categoria': final_prediction
+    }
+
+    return jsonify(response)
